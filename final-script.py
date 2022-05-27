7d8ca2f9f356cbe02e19916628a4dbe4ad58cf27
rabucejo <rogerhapay@gmail.com>
2022-05-27 12:29:44 +0800

    finalize structures

--- final-script.py
+++ final-script.py
@@ -10,12 +10,76 @@ from imutils.video import FPS
 import face_recognition
 import imutils
 import time  
+#rasberry pi imports
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+import requests
+import serial
+import os, time
+from tkinter import *
+
+#oled display Imports
+import time
+import board
+import busio
+from pathlib import Path
+from luma.core.interface.serial import i2c, spi, pcf8574
+from luma.core.interface.parallel import bitbang_6800
+from luma.core.render import canvas
+from luma.oled.device import ssd1306, ssd1309, ssd1325, ssd1331, sh1106, ws0010
+from PIL import ImageFont
+
+
+#GPIO.setmode(GPIO.BOARD)
+port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
+reader = SimpleMFRC522()
 # Open the camera, 0 is the default camera,
 #1 is the external camera, 2 is the usb camera
 cam = cv2.VideoCapture(0)
 #Enter your name
 name = input('Enter your name: ')
 names = []
+
+#OLED DISPLAY, Initialization/Setup
+def oled_disp(text):
+    font = ImageFont.load_default()
+    font_mc = ImageFont.truetype('/home/admin/Desktop/scripts/Arialn.ttf', 32)
+    font_mcsmall = ImageFont.truetype(
+        '/home/admin/Desktop/scripts/Arialn.ttf', 14)
+    serial = i2c(port=1, address=0x3C)
+    device = sh1106(serial)
+    x = 0
+    top = 0
+    width = device.width
+    height = device.height
+    with canvas(device) as draw:
+        draw.text((x, top),  text,  font=font_mcsmall, fill=255)
+
+#SIM900A
+def sendMessage(phone):
+    port.write(b'AT\r')
+    rcv = port.read(10)
+    time.sleep(1)
+    port.write(b"AT+CMGF=1\r")
+    print("Text Mode Enabled…")
+    oled_disp("Text Mode Enabled…")
+    time.sleep(3)
+    a = b'AT+CMGS="'
+    b = b'"\r'
+    print(a+phone+b)
+    port.write(a+phone+b)
+    msg = " xxx Your son/daughter scanned his/her RFID. He/she is now inside the campus. xxx"
+    print("sending message….")
+    oled_disp("sending message...")
+    time.sleep(3)
+    port.reset_output_buffer()
+    time.sleep(1)
+    port.write(str.encode(msg+chr(26)))
+    time.sleep(3)
+    print("message sent…")
+    oled_disp("message sent…")
+    time.sleep(0.5)
+#datasets
 def generate_dataset():
     cv2.namedWindow("press space to take a photo", cv2.WINDOW_NORMAL)
     cv2.resizeWindow("press space to take a photo", 500, 400)
@@ -49,7 +113,7 @@ def generate_dataset():
             imgCounter += 1
     cam.release()
     cv2.destroyAllWindows()
-
+#training model
 def train_model():
     # our images are located in the dataset folder
     print("[!!!] processing faces...")
@@ -93,7 +157,7 @@ def train_model():
     f = open("encodings.pickle", "wb")
     f.write(pickle.dumps(data))
     f.close()
-
+#face recognition
 def facial_rec():
     #Initialize 'defaultName' to trigger only when a new person is identified.
     defaultName = "unknown"
